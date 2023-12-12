ccea982cba536836e4191a3d41a7135ae0266e91
DanielaCanas <danielacanasu@gmail.com>
2023-12-12 11:33:38 -0400

    Nuevos comentarios al código ESP32
    Comentarios al código para entenderlos mejor, hecho por Joaquín

--- src/main.cpp
+++ src/main.cpp
@@ -4,58 +4,74 @@
 
 BluetoothSerial SerialBT;
 
-//Hola
+// Pines seguros CAM: 2, 12, 13, 14, 15, 16
+// Nombre de los pines de los motores
+// (MR=Right Motor) (ML=Left Motor) (1=Forward) (2=Backward)
 
-//Pines libres 4, 13, 12, 15, 14
+// ********** DECLARACIÓN DE PINES **********
 
-//Pico pins (MR=Right Motor) (ML=Left Motor) (1=Forward) (2=Backward)
+// Pines para el sensor ultrasonico
 
-const int TRIG_Pin = 19;      // Pines del para el sensor ultrasonico
+const int TRIG_Pin = 19;
 const int ECHO_Pin = 21;
+
+// Pines para los infrarrojos
 const int INFRAIZQ_Pin = 18; //Pines para los sensores
 const int INFRADER_Pin = 5;
+
+// Pines de comunicación con el Pico 
+// (U2 en el ESP32, para que no colisione
+// con la comunicación por U1 con la computadora
+// para debugging)
+
 const int RXD1 = 16;
 const int TXD1 = 17;
 
-char receivedChar;           // received value will be stored as CHAR in this variable
-int Luces_bit = 0;           //Estado de las luces
-float tiempo_espera;          //Salida sensor ultrasonico
-float distancia;             //Distancia medida por el ultrasonico
-int modo = 0;           //En 0 es manual y 1 es automatico
+// **************** VARIABLES ****************
+char receivedChar;           // Caracter recibido por la aplicación
+int Luces_bit = 0;           // Estado de las luces (0 == OFF; 1 == ON)
+int Buzzer_bit = 0;          // Estado del buzzer (0 == OFF; 1 == ON)
+
+// Estado de los infrarrojos
 int INFRAIZQ_bit;
 int INFRADER_bit;
-int Buzzer_bit = 0;
-uint32_t mseg_Ultrasonico = 0;
-uint32_t mseg_Buzzer = 0;
+// (0 == detecta; 1 == no detecta)
 
+float tiempo_espera;         //Salida sensor ultrasonico
+float distancia;             //Distancia medida por el ultrasonico  (Luego de convertir tiempo a distancia)
+int modo = 0;                //Modo de operación del carrito. 0 es manual y 1 es automático
 
+uint32_t mseg_Ultrasonico = 0;  // Momento entre la última medida del ultrasónico y la próxima
+uint32_t mseg_Buzzer = 0;       // Momento desde el encendido del buzzer
 
-//Rutina de lectura del ultrasonico
+// ***************** RUTINAS *****************
 
-void ultrasonico(); 
-float timeToCm(float time);
+void ultrasonico();         // Rutina de lectura del ultrasónico
+float timeToCm(float time); // Rutina auxiliar para el ultrasónico
 
 void setup() {
-  Serial.begin(115200);
-  SerialBT.begin("ESP32 CAR"); //Nombre del dispositivo Bluetooth
+  Serial.begin(115200);     // Inicio de la comunicación serial para debugging
+  SerialBT.begin("ESP32 CAR"); // Nombre del dispositivo Bluetooth
   Serial1.begin(115200, SERIAL_8N1, RXD1, TXD1); //Inicio de la comunicación serial con el pico
   
+  // Inicialización de los pines
+
   pinMode(ECHO_Pin, INPUT);
   pinMode(TRIG_Pin, OUTPUT);
   pinMode(INFRAIZQ_Pin, INPUT);
   pinMode(INFRADER_Pin, INPUT);
 }
 
-
 void loop() {
-  if(millis() - mseg_Ultrasonico >= 700) {
-    ultrasonico(); //Mide distancia
-    mseg_Ultrasonico = millis();
+  // Rutina de lectura del ultrasónico
+  if(millis() - mseg_Ultrasonico >= 700) { // Si pasa el tiempo apropiado desde la última lectura...
+    ultrasonico();                        // Haz la rutina del ultrasónico
+    mseg_Ultrasonico = millis();          // Actualiza el momento de la última lectura
   }
   
+  // Rutina de lectura de los infrarrojos
   INFRADER_bit = digitalRead(INFRADER_Pin);
   INFRAIZQ_bit = digitalRead(INFRAIZQ_Pin);
-
 
   if (modo) { //Automatico
     if(!INFRAIZQ_bit && !INFRADER_bit) {
@@ -68,36 +84,54 @@ void loop() {
       Serial1.write('S');
     }
   }
-
-  if (Serial.available()) {
-    SerialBT.write(Serial.read());
+  // Este if sirve para enviar comandos desde la computadora hacia la aplicación
+  if (Serial.available()) {        // Si se envían datos desde la computadora...
+    SerialBT.write(Serial.read()); // Se reenvían directo a la app
   }
 
-  if (SerialBT.available()) {
+  // Rutina de lectura de los comandos enviados desde la app
+  if (SerialBT.available()) { // Si se envía un caracter desde la app
     
     receivedChar =(char)SerialBT.read();
-    //Serial1.write(receivedChar); //Envio a RasberryPi Pico
+    //Serial1.write(receivedChar); // Envio DIRECTO al Pico ## DA PROBLEMAS ##
 
+    // Los caracteres de la app tienen al final un salto de línea
+    // El objetivo de este if es que la información del debugging
+    // Se vea lo más bonita posible
     if(receivedChar != '\n') {
       Serial.print ("Received:");//print on serial monitor
       Serial.println(receivedChar);//print on serial monitor 
       }
 
-     if(receivedChar == 'M') {
+    if(receivedChar == 'M') {  // Luces
       Serial1.write('M');
-     }
+    }
     
     if(receivedChar == 'Y') { //Modo del carro
       if (modo) {
         modo = 0;
-        Serial1.write('S');
+        Serial1.write('S'); // Si el carro pasa de automático a manual, que se detenga
       }
       else {
         modo = 1;
       } 
     }
-    
-    if (!modo) { //Manual
+
+    // Corneta
+    //
+    // Se puso la condición "!Buzzer_bit" porque el buzzer
+    // Es temporizado, y no se quiere que se envíe otro caracter X
+    // Cuando el buzzer ya está sonando
+
+    if (receivedChar == 'X' && !Buzzer_bit) { //Corneta
+      Serial1.write('X');
+      Buzzer_bit = 1;
+      mseg_Buzzer = millis();
+    }
+    // En este if se manejan todos los caracteres
+    // que se refieren a movimiento de las ruedas
+
+    if (!modo) { // Si el modo es manual...
       if(receivedChar == 'F') {
         Serial1.write('F');
       }
@@ -126,34 +160,33 @@ void loop() {
         Serial1.write('S');
       }
     }
-    
-    if (receivedChar == 'X' && !Buzzer_bit) { //Corneta
-      Serial1.write('X');
-      Buzzer_bit = 1;
-      mseg_Buzzer = millis();
+  }
+   // Aquí se controla el tiempo de encendido del buzzer
+  if ((Buzzer_bit)) { // Si el buzzer está encendido
+    if(millis() - mseg_Buzzer >= 1000 ) { // Si pasa el tiempo suficiente desde el encendido del buzzer...
+      char send = 'X'; 
+      Serial1.write(send); // Apagalo
+      Buzzer_bit = 0; // Y actualiza la bandera de estado correspondiente
     }
   }
-    if ((Buzzer_bit)) {
-      if(millis() - mseg_Buzzer >= 1000 ) {
-        char send = 'X';
-        Serial1.write(send);
-        Buzzer_bit = 0;
-      }
-    }
   delay(20);
 }
 
 void ultrasonico() {
+  // Se manda el pulso al ultrasónico
   digitalWrite (TRIG_Pin, LOW); 
   delayMicroseconds(2);
   digitalWrite (TRIG_Pin, HIGH);
   delayMicroseconds (10);
   digitalWrite (TRIG_Pin, LOW); 
 
+  // Se mide la respuesta del ultrasónico
   tiempo_espera = pulseIn(ECHO_Pin, HIGH, 20);
 
+  // Se transforma la respuesta a unidad de distancia
   distancia = timeToCm(tiempo_espera);
-
+  
+  // Se manda la información a la app
   SerialBT.print('D');  
   SerialBT.println(distancia);  
 }
